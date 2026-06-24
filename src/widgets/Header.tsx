e2c2d5403ccf8975d8dbe1c4b0829188b8b7c867
chaneul <chaneul@worshiponair.com>
2026-06-24 10:14:38 -0700

    add red live dot indicator

--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -5,7 +5,10 @@ export default function Header() {
   return (
     <header className="stream-header">
       <div className="stream-header__top">
-        <span className="stream-header__live">LIVE</span>
+        <span className="stream-header__live">
+          <span className="stream-header__live-dot" aria-hidden="true" />
+          LIVE
+        </span>
 
         <LanguageSelector />
       </div>
